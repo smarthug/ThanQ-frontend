0de5a64dde3081cf0f5d6552c7233203c30e3f15
smarthug <kirklayer@gmail.com>
2024-11-16 18:25:17 +0900

    Refactor CreateQueueInputs component to simplify booth registration logic and remove unused contract addresses

--- src/components/mui/CreateQueueInputs.jsx
+++ src/components/mui/CreateQueueInputs.jsx
@@ -2,20 +2,8 @@ import React, { useState } from 'react';
 import { Container, TextField, Button, Typography, Box } from '@mui/material';
 import { styled } from '@mui/system';
 
-// import { useRegisterBooth } from '../../hooks/useRegisterBooth';
+import { useRegisterBooth } from '../../hooks/useRegisterBooth';
 
-
-import { abi } from "./abi.json";
-import { useWriteContract } from 'wagmi';
-
-const CONTRACT_ADDRESSES = {
-  80002: "0xc1201A262aD48b0EaF44421E13eB22c0a8ab4368", // Polygon Mumbai
-  534351: "0x5c59052D1853145DDe9bc462b61302C252FCc5df", // Scroll Testnet
-  84532: "0xC80a7FDd3A63fc5666213a5a0fE5a3681369DE70", // Base Testnet
-  25925: "0xff23b48Cd4A045eD210435FaDF54352C09F985c8", // Bitkub Testnet
-  545: "0xff23b48Cd4A045eD210435FaDF54352C09F985c8", // Flow Testnet
-  296: "0x6A5ABe9DECccB57170d9C43A3a5028aa9104AC41", // Hedera Testnet
-};
 
 const StyledContainer = styled(Container)({
   display: 'flex',
@@ -44,13 +32,13 @@ const PreviewImage = styled('img')({
 
 const CreateQueue = () => {
 
-  const { writeContract } = useWriteContract()
   const [name, setName] = useState('fefef');
   const [maxParticipant, setMaxParticipant] = useState('');
   const [image, setImage] = useState(null);
-
-
+  
+  
   const baseURI = 'https://api.example.com';
+  const registerBooth = useRegisterBooth(name, baseURI);
   // const { write, isLoading, isSuccess, isError } = useRegisterBooth(name, baseURI);
 
   const handleImageChange = (e) => {
@@ -60,13 +48,13 @@ const CreateQueue = () => {
   const handleSubmit = () => {
     // Here, you would handle the form submission, such as making an API call
     console.log('Queue Created:', { name, maxParticipant, image });
-
-    writeContract({
-      abi,
-      address: CONTRACT_ADDRESSES[545],
-      functionName: 'registerBooth',
-      args: [name, baseURI],
-    })
+    registerBooth();
+    // writeContract({
+    //   abi,
+    //   address: CONTRACT_ADDRESSES[545],
+    //   functionName: 'registerBooth',
+    //   args: [name, baseURI],
+    // })
   };
 
 
