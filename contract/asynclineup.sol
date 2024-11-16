56e8f47303de543b094b7e1dc6dc8d0c94369611
smarthug <kirklayer@gmail.com>
2024-11-17 05:24:32 +0700

    Add functions to retrieve queue status and user positions in the BoothQueue contract

--- contract/asynclineup.sol
+++ contract/asynclineup.sol
@@ -117,5 +117,91 @@ contract BoothQueue is ERC721, Ownable {
             emit NextInLine(boothId, currentUser);
             booth.nextInLine += 1;
         }
+        
+         //get all the amount of queue& current processing address
+    function getAllWaitingAmount() public view returns (uint256[] memory, address[] memory, address[] memory) {
+        uint256[] memory waitingAmounts = new uint256[](boothCounter);
+        address[] memory currentProcessingAddresses = new address[](boothCounter);
+        address[] memory nextInLineAddresses = new address[](boothCounter);
+
+        for (uint256 i = 0; i < boothCounter; i++) {
+            Booth storage booth = booths[i];
+            
+            uint256 queueWaiting = booth.nextAvailableId - booth.nextInLine;
+            uint256 postponedWaiting = booth.endPointPostponed - booth.nextPostponed;
+            
+            waitingAmounts[i] = queueWaiting + postponedWaiting;
+            currentProcessingAddresses[i] = booth.currentProcessing;
+
+            // 다음 처리할 주소를 결정: postponed queue가 있으면 해당 주소, 아니면 nextInLine의 주소
+            if (booth.postponed[booth.nextPostponed] != address(0)) {
+                nextInLineAddresses[i] = booth.postponed[booth.nextPostponed];
+            } else {
+                nextInLineAddresses[i] = booth.queue[booth.nextInLine];
+            }
+        }
+
+        return (waitingAmounts, currentProcessingAddresses, nextInLineAddresses);
+    }
+
+    //users can get thier own queue positions
+    function getUserQueuePositions(address user) public view returns (uint256[] memory) {
+        uint256[] memory queuePositions = new uint256[](boothCounter);
+
+        for (uint256 i = 0; i < boothCounter; i++) {
+            Booth storage booth = booths[i];
+
+            uint256 userWaitingNumber = booth.waiting[user];
+            if (userWaitingNumber > 0) { // 사용자가 대기 중인 경우
+                uint256 positionInQueue = (userWaitingNumber - booth.nextInLine) + 
+                                        (booth.endPointPostponed - booth.nextPostponed);
+                queuePositions[i] = positionInQueue;
+            } else {
+                queuePositions[i] = 0; // 사용자가 대기 중이지 않은 경우
+            }
+        }
+
+        return queuePositions;
+    }
+
+    
+    // 토큰 URI 설정
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        address owner = ownerOf(tokenId);
+        uint256 boothId;
+        
+        // 사용자 별 보유한 부스 ID 확인
+        for (uint256 i = 0; i < userBooths[owner].length; i++) {
+            if (userTokens[userBooths[owner][i]][owner] == tokenId) {
+                boothId = userBooths[owner][i];
+                break;
+            }
+        }
+
+        Booth storage booth = booths[boothId];
+        return string(abi.encodePacked(booth.baseURI, uint2str(tokenId), ".json"));
+    }
+
+    // uint256을 string으로 변환하는 함수
+    function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
